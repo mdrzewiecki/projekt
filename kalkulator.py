8d4acbb75beae5c7ff4358b55a85ed620cc97c92
Michał Drzewiecki <37240@student.ue.poznan.pl>
2016-03-23 12:57:20 +0100

    dodalem funkcje get info

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,12 @@
-def dodawanie(a, b):
-    return a + b
 
+def dodawanie():
+    a = int (input("podaj a"))
+    b = int(input("podaj b"))
+    print (a+b)
 
+def get_info():
+    return "to jest program kalkulator stworzony przez Michala"
+
+print (get_info())
 dodawanie()
 input()
